20b17de09672ec334d3152771d6839257efedb34
강호진 <battlemech@naver.com>
2024-02-15 17:10:51 +0900

    msw 추가

--- cycloid/src/main.js
+++ cycloid/src/main.js
@@ -6,9 +6,24 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+async function prepareApp() {
+  if (
+    process.env.NODE_ENV === 'development' ||
+    process.env.NODE_ENV === 'test'
+  ) {
+    const { worker } = await import('./mocks/browser')
+    return worker.start()
+  }
+
+  return Promise.resolve()
+}
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
-app.mount('#app')
+prepareApp().then(() => {
+  app.mount('#app')
+})
+
---
+++ cycloid/src/mocks/handlers.js
@@ -0,0 +1,10 @@
+import { http, graphql, HttpResponse } from 'msw'
+
+export const handlers = [
+  http.get('https://api.example.com/user', () => {
+    return HttpResponse.json({
+      firstName: 'John',
+      lastName: 'Maverick',
+    })
+  }),
+]
