b2773fc1b2085a45aa13975bf1a29b277b0bf3bb
강호진 <battlemech@naver.com>
2024-02-15 21:29:23 +0900

    mainview, file pinia 작성 중

--- cycloid/src/stores/file.js
+++ cycloid/src/stores/file.js
@@ -7,19 +7,19 @@ export const useFileStore = defineStore('file', {
   }),
   actions: {
     setFileList(file) {
-      // this.files.push(file);
-      // console.log('list!', this.files);
-      // console.log('size!', this.files[0].size);
       console.log('file!,', file);
       const formData = new FormData()
-      formData.append('file', file);
+      formData.append('files', file);
       axios.post('https://api.example.com/upload', 
         formData,
         {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
-      });
+      })
+      .then((response) => {
+        console.log('response', response);
+      })
     },
   }
 })
--- cycloid/src/views/MainView.vue
+++ cycloid/src/views/MainView.vue
@@ -6,10 +6,13 @@
         logo
       </div>
       <div class="trashbox-button">
-
+        <a href="/trashbox">삭제 리스트</a>
       </div>
       <div class="upload-button">
-
+        <label for="file">
+          <div class="upload-button active-button">이미지, 영상 업로드</div>
+        </label>
+        <input type="file" name="file" id="file" ref="upload_file" accept="image/*, video/*" @change="uploadFile">
       </div>
     </div>
     <div class="main-body">
@@ -26,22 +29,59 @@
       </div>
     </div>
   </div>
-  <div>dsf</div>
 </template>
 
 <script>
+import axios from 'axios';
+import { useFileStore } from '../stores/file.js';
+
 export default {
   data() {
     return {
+      fileStore: useFileStore(),
 
+      user: "",
     }
   },
+  mounted() {
+    console.log('main view created');
+    this.getUser();
+    console.log(this.user);
+
+    this.getItemList();
+  },
   methods: {
+
+    // msw test
+    async getUser() {
+      axios.get('https://api.example.com/user')
+        .then((response) => {
+          this.user = response.data;
+        })
+    },
+    // 
+
+    uploadFile() {
+      console.log('upload file');
+      let file = this.$refs.upload_file.files[0];
+      console.log('file!', file);
+      this.fileStore.setFileList(file);
+    },
+    getItemList() {
+      axios.get('https://api.example.com/itemlist')
+      .then((response) => {
+        console.log('item list', response.data);
+      })
+    },
+  },
+  computed: {
     
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+#file {
+  display: none;
+}
 </style>
